e90734741d9f4e260cc689a39967e1727b9c60f7
eastrd <e0t3rx@gmail.com>
2024-07-03 20:27:14 +1000

    Add Domain To IP script

---
+++ domain/domain_to_ip.py
@@ -0,0 +1,21 @@
+import socket
+import sys
+import json
+
+def get_ip_from_domain(domain):
+    try:
+        ip = socket.gethostbyname(domain)
+        return ip
+    except socket.gaierror:
+        return "Error: Unable to resolve domain"
+
+ip = get_ip_from_domain(sys.argv[1])
+
+print(
+    json.dumps(
+        {
+            "type": "Tree",
+            "results": [f"## {ip}"],
+        }
+    )
+)
