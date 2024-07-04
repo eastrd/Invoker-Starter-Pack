cae465914761d522e39f5f689270740f5df0dc69
eastrd <e0t3rx@gmail.com>
2024-07-04 15:01:30 +1000

    Update nmap script to produce proper network output

--- scan/nmap.py
+++ scan/nmap.py
@@ -1,6 +1,7 @@
 import subprocess
 import sys
 import json
+import xml.etree.ElementTree as ET 
 
 def execute_command(cmd):
     try:
@@ -11,14 +12,48 @@ def execute_command(cmd):
     except subprocess.CalledProcessError as e:
         return e.output
 
+def parse_nmap_xml(xml_content):
+    root = ET.fromstring(xml_content)
+    results = []
+    # Find all hosts
+    for host in root.findall('./host'):
+        address = host.find('address').get('addr')
 
-scan_results = execute_command(f"nmap -sV -T4 -p- --top-ports 1000 {sys.argv[1]}")
+        address_content = f"## {address}"
+        results.append({address_content: []})
+        
+        # Find all port elements for this host
+        for port in host.findall('./ports/port'):
+            port_id = port.get('portid')
+            protocol = port.get('protocol')
+            
+            state = port.find('state').get('state')
+            
+            service = port.find('service')
+            if service is not None:
+                service_name = service.get('name')
+                product = service.get('product', '')
+                version = service.get('version', '')
+            else:
+                service_name = ''
+                product = ''
+                version = ''
+            
+            results[-1][address_content] = [f"""### Port: {port_id}/{protocol}""" \
+                        + f"\n- Service: {service_name}" if service_name else "" \
+                        + f"\n- Product: {product}" if product else "" \
+                        + f"\n- Version: {version}" if version else ""]
+    return results
+
+
+scan_result_xml = execute_command(f"nmap -sV -T4 -p- --top-ports 1000 -oX - {sys.argv[1]}")
+results = parse_nmap_xml(scan_result_xml)
 
 print(
     json.dumps(
         {
             "type": "Tree",
-            "results": [f"```\n{scan_results}\n```"],
+            "results": results,
         }
     )
 )
