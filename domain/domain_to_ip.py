9362107cb6ba4368e9feaecd48e4c1028db7880e
eastrd <e0t3rx@gmail.com>
2025-05-20 23:24:23 +1000

    Add error logging

--- domain/domain_to_ip.py
+++ domain/domain_to_ip.py
@@ -2,12 +2,14 @@ import socket
 import sys
 import json
 
+
 def get_ip_from_domain(domain):
     try:
         ip = socket.gethostbyname(domain)
         return ip
     except socket.gaierror:
-        return "Error: Unable to resolve domain"
+        return "Error: Unable to resolve domain: " + str(domain)
+
 
 ip = get_ip_from_domain(sys.argv[1])
 
