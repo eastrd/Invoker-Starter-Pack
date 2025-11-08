8ecae54a93f2bbad6868999d805a649ce42b22f8
eastrd <e0t3rx@gmail.com>
2025-11-08 11:29:33 +1100

    Fix parser

--- domain/whois.py
+++ domain/whois.py
@@ -3,13 +3,15 @@ import sys
 import json
 from bs4 import BeautifulSoup as bs
 
+
 def get_whois(domain):
     url = f"https://www.whois.com/whois/{domain}"
     res = rq.get(url)
-    soup = bs(res.content, 'html.parser')
+    soup = bs(res.content, "html.parser")
     # Extract raw WHOIS data
-    raw_data = soup.find('pre', class_='df-raw')
-    return raw_data.text.strip()
+    rows = soup.find_all("div", class_="df-row")
+    return "\n".join(row.text.strip() for row in rows)
+
 
 domain = sys.argv[1]
 whois_info = get_whois(domain)
